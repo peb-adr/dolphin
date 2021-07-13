9f223d46a1129302740f10845850c63d8aa15100
Adrian Richter <adrichter97@gmail.com>
2021-07-13 22:54:20 +0200

    nil check

--- Data/Scripts/bfbb_capStickDeflection.lua
+++ Data/Scripts/bfbb_capStickDeflection.lua
@@ -1,7 +1,8 @@
 local core = require "bfbb_core"
 
+
 function onScriptStart()
-	
+
 end
 
 function onScriptCancel()
@@ -9,10 +10,8 @@ function onScriptCancel()
 end
 
 function onScriptUpdate()
-    ------------------------------------------------------
-	-- WORKING DEFLECTION CAP USING SMILEYS QUICK MATHS --
-	------------------------------------------------------
-	
+	-- credits to Smiley for doing the math
+
 	local cap = require "opt.bfbb_capStickDeflection"
 	xIn = GetMainStickX() - 128
 	yIn = GetMainStickY() - 128
@@ -21,18 +20,13 @@ function onScriptUpdate()
 	y = yIn / length
 
 	m = math.pi / 4
-	a = core.atan2(y,x) % (2 * m)
+	a = core.atan2(y,x)
+	if a == nil then
+		return
+	end
+	a = a % (2 * m)
 	if a > m then a = 2 * m - a end
 	s = cap / math.cos(a)
-
-	-- x = x * s
-	-- y = y * s
-
-	-- x = (x * 128) + 128
-	-- y = (y * 128) + 128
-	
-	-- x = math.floor(x)
-	-- y = math.floor(y)
 
 	xOut = math.floor(s * x * 128)
 	yOut = math.floor(s * y * 128)
